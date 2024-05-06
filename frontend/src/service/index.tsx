0ab42f8758aefc3f766c07af9eed6bcfc482d74c
somanth-more <somnath.more@zemosolabs.com>
2024-05-06 09:07:00 +0530

    fix : Update localhost

--- frontend/src/service/index.tsx
+++ frontend/src/service/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'http://51.20.68.205:8000/',
 });
 
 export const getAqi = async (city: any) => {
@@ -11,5 +11,5 @@ export const getAqi = async (city: any) => {
 };
 
 export const getSkills = async (name: any) => {
-  return axios.get(`http://localhost:8000/v1/skills/${name}`);
+  return axios.get(`http://51.20.68.205:8000/v1/skills/${name}`);
 };
--- frontend/src/service/setJobCard.tsx
+++ frontend/src/service/setJobCard.tsx
@@ -4,7 +4,7 @@ export const setJobCard = async (url: string) => {
   const response = await API.post(url, {
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
-      'Access-Control-Allow-Origin': 'http://localhost:8000',
+      'Access-Control-Allow-Origin': 'http://51.20.68.205:8000',
     },
   });
   return response.data;
